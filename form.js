db7eeb51a5c12eae350901f4a4e7dd665e18429f
easysajusaju-dev <easysaju.saju@gmail.com>
2025-11-10 14:19:34 +0900

    Update form.js

--- form.js
+++ form.js
@@ -7,7 +7,7 @@ const pageLoadTime = new Date();
 
 // ✅ Google Apps Script WebApp 주소 (시트 기록용)
 const APPS_SCRIPT_URL =
-  https://script.google.com/macros/s/AKfycbzhi4556hgBKctN3KVBlPdkl1vFD3oG7Wv7Hdm6pk16VGG8OF6q6EaPT8t_5WTX87Jb/exec";
+  "https://script.google.com/macros/s/AKfycbzhi4556hgBKctN3KVBlPdkl1vFD3oG7Wv7Hdm6pk16VGG8OF6q6EaPT8t_5WTX87Jb/exec";
 
 /* ------------------------------
  * 공통 UI 헬퍼
